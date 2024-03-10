804f0721ec5b6d6ba4428ccd76255992d31ad9fb
t1rin <t1rin@mail.ru>
2024-03-10 18:27:02 +0400

    Added help command + created config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+INPUT_ERROR_TEXT = "Команда не найдена. help для получения дополнительной информации"
+
+GLOBAL_HELP_TEXT = """
+global commands:
+  list возвращает все существующие предметы
+  help возвращает то, что вы читаете
+  exit закрывает программу
+"""
+
+LOCAL_HELP_TEXT = """
+local commands:
+  exit выходит в глобальную среду
+"""
+
+PROGRAM_NAME = "taskcontrol-command"
+
+GLOBAL_NAME = "~"
--- taskcontrol.py
+++ taskcontrol.py
@@ -1,32 +1,47 @@
-import sys
-
 from subject import *
+from config import *
 
 current_subject = None
 
 
 def input_error() -> None:
-    ...
+    print(INPUT_ERROR_TEXT)
+
+
+def _command_help() -> None:
+    if not current_subject:
+        print(GLOBAL_HELP_TEXT)
+    else:
+        print(LOCAL_HELP_TEXT)
 
 
 def main() -> None:
+    global current_subject
     while True:
-        commands = input("command:" + (current_subject if current_subject else "~") + "$ ").split()
+        commands = input(PROGRAM_NAME + ":" + (current_subject if current_subject else GLOBAL_NAME) + "$ ").split()
+        if len(commands) == 0:
+            continue
         if not current_subject:  # global
             match commands[0]:
                 case "list":
-                    ...
+                    for subject in subject_list():
+                        print(subject)
+                case "help":
+                    _command_help()
+                case "exit":
+                    exit(0)
                 case _:
                     input_error()
         else:  # local
             match commands[0]:
                 case "exit":
-                    ...
+                    current_subject = None
                 case _:
                     input_error()
 
 
 if __name__ == "__main__":
+    import sys
     if len(sys.argv) > 1:
         current_subject = sys.argv[1]
     main()
