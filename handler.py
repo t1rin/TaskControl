44ee3128acb0596cf125cfaaff90131dfff6d7a4
t1rin <t1rin@mail.ru>
2024-03-18 22:19:57 +0400

    added handler.py; used rich lib

---
+++ handler.py
@@ -0,0 +1,36 @@
+import re
+
+from rich import print
+
+styles = {
+    "default": "white",  # default
+    "h1": "bold green",  # program name style
+    "h2": "bold yellow",  # arguments
+    "h3": "white underline",  # headings
+    "h4": "bold blue",  # name subject style
+    "h5": "bold",  # important words
+    "cmd": "green",  # command
+    "comment": "italic gray70",  # comments
+    "warning": "yellow"  # warning messages
+}
+
+
+def _format_text(match) -> str:
+    name = match.group(1)
+    text = match.group(2)
+    if name in styles.keys():
+        style = styles[name]
+    else:
+        # style = styles["default"]
+        style = name
+    return f"[{style}]{text}[/]"
+
+
+def format_text(text: str) -> str:
+    pattern = r"\[(.*?)\](.*?)\[/\1\]"
+    return re.sub(pattern, _format_text, text)
+
+
+def show(*args, **kwargs) -> None:
+    result = [format_text(arg) if isinstance(arg, str) else arg for arg in args]
+    print(*result, **kwargs)
--- taskcontrol.py
+++ taskcontrol.py
@@ -1,4 +1,5 @@
 from subject import *
+from handler import *
 from config import *
 
 import time
@@ -8,37 +9,37 @@ current_subject: Subject | None = None
 
 def _help_command() -> None:
     if not current_subject:
-        print(GLOBAL_HELP_TEXT)
+        show(GLOBAL_HELP_TEXT)
     else:
-        print(LOCAL_HELP_TEXT)
+        show(LOCAL_HELP_TEXT)
 
 
 def _create_command(commands: list[str]) -> None:
     if len(commands) < 3:
-        print(NOT_ARGS_ERROR)
+        show(NOT_ARGS_ERROR)
     else:
         if not subject_exist(commands[1]):
             try:
                 create_subject(commands[1], int(commands[2]))
             except ValueError:
-                print(ARGS_ERROR)
+                show(ARGS_ERROR)
         else:
-            print(ITEM_EXIST_ERROR)
+            show(ITEM_EXIST_ERROR)
 
 
 def _choice_command(commands: list[str]) -> None:
     if len(commands) < 2:
-        print(NOT_ARGS_ERROR)
+        show(NOT_ARGS_ERROR)
     else:
         if subject_exist(commands[1]):
             global current_subject
             current_subject = Subject(commands[1])
         else:
-            print(NOT_FOUND_ERROR)
+            show(NOT_FOUND_ERROR)
 
 
 def _list_command() -> None:
-    print(*subject_list(), sep=SEPARATOR)
+    show(*subject_list(), sep=SEPARATOR)
 
 
 def _fav_command(commands: list[str]) -> None:
@@ -46,16 +47,16 @@ def _fav_command(commands: list[str]) -> None:
         try:
             current_subject.add_to_fav(*map(int, commands[1:]))
         except ValueError:
-            print(ARGS_ERROR)
+            show(ARGS_ERROR)
     else:
-        print(*current_subject.favorite, sep=SEPARATOR)
+        show(*current_subject.favorite, sep=SEPARATOR)
 
 
 def _del_fav_command(commands: list[str]) -> None:
     try:
         current_subject.del_fav(*map(int, commands[1:]))
     except ValueError:
-        print(ARGS_ERROR)
+        show(ARGS_ERROR)
 
 
 def _done_command(commands: list[str]) -> None:
@@ -63,28 +64,28 @@ def _done_command(commands: list[str]) -> None:
         try:
             current_subject.add_to_done(*map(int, commands[1:]))
         except ValueError:
-            print(ARGS_ERROR)
+            show(ARGS_ERROR)
     else:
         for number in current_subject.solved:
             tm = time.strftime(TIME_FORMAT, time.localtime(number[1]))
-            print(f"[{tm}]{SEPARATOR}{number[0]}")
+            show(f"[{tm}]{SEPARATOR}{number[0]}")
 
 
 def _rand_command(commands: list[str]) -> None:
     if len(commands) < 2:
-        print(NOT_ARGS_ERROR)
+        show(NOT_ARGS_ERROR)
     else:
         try:
             quantity = int(commands[1])
-            print(*map(str, current_subject.rand_tasks(quantity)), sep=SEPARATOR)
+            show(*map(str, current_subject.rand_tasks(quantity)), sep=SEPARATOR)
         except ValueError:
-            print(ARGS_ERROR)
+            show(ARGS_ERROR)
 
 
 def _info_command(commands: list[str]) -> None:
     if len(commands) < 2:
         percent = len(current_subject.solved) / current_subject.quantity * 100
-        print(INFO.format(
+        show(INFO.format(
             current_subject.name,
             len(current_subject.favorite),
             len(current_subject.solved),
@@ -102,23 +103,25 @@ def _info_command(commands: list[str]) -> None:
                         found = True
                         time_n = tm
                         break
-                print(INFO_TASK.format(
+                show(INFO_TASK.format(
                     number,
-                    "+" if found else "-",
-                    "+" if number in current_subject.favorite else "-",
-                    "-" if not found else time.strftime(TIME_FORMAT, time.localtime(time_n))
+                    YES if found else NO,
+                    YES if number in current_subject.favorite else NO,
+                    NO if not found else time.strftime(TIME_FORMAT, time.localtime(time_n))
                 ))
             else:
                 raise ValueError
         except ValueError:
-            print(ARGS_ERROR)
+            show(ARGS_ERROR)
 
 
 def main() -> None:
     global current_subject
     while True:
-        text = PROGRAM_NAME + ":" + (GLOBAL_NAME if not current_subject else current_subject.name) + "$ "
-        commands = input(text).split()
+        path = GLOBAL_NAME if not current_subject else current_subject.name
+        text = f"{PROGRAM_NAME}:[{NAME_SUBJ_STYLE}]{path}[/{NAME_SUBJ_STYLE}]$ "
+        show(text, end="")
+        commands = input().split()
         if len(commands) == 0:
             continue
         if not current_subject:  # global
@@ -134,7 +137,7 @@ def main() -> None:
                 case "exit":
                     exit(0)
                 case _:
-                    print(INPUT_ERROR)
+                    show(INPUT_ERROR)
         else:  # local
             match commands[0]:
                 case "help":
@@ -152,7 +155,7 @@ def main() -> None:
                 case "exit":
                     current_subject = None
                 case _:
-                    print(INPUT_ERROR)
+                    show(INPUT_ERROR)
 
 
 if __name__ == "__main__":
